bf20f39072a56dc7790bf5c60f5a6f31f8f60372
Pat Carter <patcarter883@gmail.com>
2019-10-08 20:59:53 +1100

    Yes No Button control added

---
+++ client/src/components/vfg/fieldQRepeater.vue
@@ -0,0 +1,16 @@
+<template>
+  <vue-form-generator
+    :schema="schema.schema"
+    :model.sync="schema.value"
+    :options="formOptions"
+  />
+</template>
+
+<script>
+import { abstractField } from 'vue-form-generator';
+
+export default {
+  name: 'fieldQInput',
+  mixins: [abstractField]
+};
+</script>
